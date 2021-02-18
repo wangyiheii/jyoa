fb72aba5784688182ac35665ffa6b447115b4b7f
yihe <yihe@qq.com>
2021-02-18 11:34:27 +0800

    添加 通用发送提示信息 文件

--- application/index/controller/Remind.php
+++ application/index/controller/Remind.php
@@ -6,7 +6,20 @@ use app\common\controller\Frontend;
 use app\index\model\Remind as Mremind;
 class Remind extends Frontend
 {
+    public function list(){
+        
+    }
     public function add_remind(){
+        $Mremind = new Mremind();
+        $param = $this->request->param();
+        if(request()->isPost()){
+            $return = $Mremind->add($param);
+            if($return){
+                return ajax_json('1','添加成功');
+            }else{
+                return ajax_json('0','添加失败');
+            }
+        }
         return $this->view->fetch();
     }
 }
